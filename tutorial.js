235e687474323cbb38e005fdb7ce4281d0dd6a1d
yuansamuelandres <yuandeslytherinandres@gmail.com>
2024-07-14 21:41:05 +0200

    Created the main folders needed for the tutorial, and added my first notes set

---
+++ tutorial.js
@@ -0,0 +1,17 @@
+//In the name of Allah
+
+console.log("log")
+console.error("error")
+console.log("Hello from %cJS File", "color: yellow; font-size: 40px;")
+
+
+
+/*
+Console Methods:
+    log
+    error
+    table
+
+Styling Console:
+    Directive %c
+*/
