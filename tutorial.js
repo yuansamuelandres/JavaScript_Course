56933332468a353a8a1bbb3b76e3054911e5136b
yuansamuelandres <yuandeslytherinandres@gmail.com>
2024-07-17 04:50:26 +0200

    Committed the second JS Challenge

--- tutorial.js
+++ tutorial.js
@@ -1,23 +1,19 @@
 // In the name of Allah
 
-console.log(+"-100")
-console.log(+null)
-console.log(+true)
-console.log(-null)
+/* Challenge 2: Operators */
+let a = 10
+let b = "20"
+let c = 80
+console.log(++a + +b++ + +c++ - +a++)
+console.log(++a + -b + +c++ - -a++ + +a)
+console.log(--c + +b + --a * +b++ - +b * a + --a - +true)
+// 11 + 20(b=21) + 80(c=81) - 11(a=12) = 100
+// 13 + [-21] + 81(c=82) - [-13](a=[14]) + 14 = 100 
+// 81 + 21 + 11 * 21(b=22) - 22 * 11 + 10 - 1 = 81 + 21 + 231 - 242 + 10 - 1 = 100
 
-console.log(true - null)
-
-let x = 5
-x += 95
-
-/* 
-    Unary Plus: returns a number
-    Unary Negative
-
-    Type Coercion (Type Casting)
-
-    Assignment Operators:
-        +=
-        -=
-        /=
-*/
+let d = "-100"
+let e = "20"
+let f = 30
+let g = true
+console.log(-d * e)         //2000
+console.log(-d + e * 2 + f + g * 3)         //173
