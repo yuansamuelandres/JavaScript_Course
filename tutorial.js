5d5fe91750b111c986d7e0515a0abce7048f9419
yuansamuelandres <yuandeslytherinandres@gmail.com>
2024-07-19 21:24:03 +0200

    Added the eigth set of JS code notes

--- tutorial.js
+++ tutorial.js
@@ -1,18 +1,32 @@
 // In the name of Allah
 
-console.log(1000000)
-console.log(1_000_000)
-console.log(1e6)
-console.log(10 ** 6)
+console.log(100..toString())
+console.log(100.156545.toFixed(2))
+console.log(parseInt("100 yes"))
+console.log(Number.isInteger(102.3))
+console.log(Number.isNaN(102.3))
 
-console.log(Number.MAX_SAFE_INTEGER)
-console.log(Number.MAX_VALUE)
-console.log(Number.POSITIVE_INFINITY)
+console.log(Math.round(99.2))
+console.log(Math.round(99.5))
+console.log(Math.ceil(99.5))
+console.log(Math.floor(99.5))
+console.log(Math.trunc(99.5))
 
+/* Number Methods:
+    toString()
+    toFixed()
+    parseInt()
+    parseFloat()
+    Number.isInteger()
+    Number.isNaN()
 
-/* Number:
-    Syntactic Sugar:
-        _
-        e
-        **
+Math Objects:
+    round()
+    ceil()
+    floor()
+    min()
+    max()
+    pow(,)
+    random()
+    trunc()
 */
