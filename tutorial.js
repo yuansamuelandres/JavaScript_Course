092c2f4f205fd6a01c73fcbbdf87cb4d2ba36dca
yuansamuelandres <yuandeslytherinandres@gmail.com>
2024-07-19 21:14:25 +0200

    Added the seventh set of JS code notes

--- tutorial.js
+++ tutorial.js
@@ -1,19 +1,18 @@
 // In the name of Allah
 
-/* Challenge 2: Operators */
-let a = 10
-let b = "20"
-let c = 80
-console.log(++a + +b++ + +c++ - +a++)
-console.log(++a + -b + +c++ - -a++ + +a)
-console.log(--c + +b + --a * +b++ - +b * a + --a - +true)
-// 11 + 20(b=21) + 80(c=81) - 11(a=12) = 100
-// 13 + [-21] + 81(c=82) - [-13](a=[14]) + 14 = 100 
-// 81 + 21 + 11 * 21(b=22) - 22 * 11 + 10 - 1 = 81 + 21 + 231 - 242 + 10 - 1 = 100
+console.log(1000000)
+console.log(1_000_000)
+console.log(1e6)
+console.log(10 ** 6)
 
-let d = "-100"
-let e = "20"
-let f = 30
-let g = true
-console.log(-d * e)         //2000
-console.log(-d + e * 2 + f + g * 3)         //173
+console.log(Number.MAX_SAFE_INTEGER)
+console.log(Number.MAX_VALUE)
+console.log(Number.POSITIVE_INFINITY)
+
+
+/* Number:
+    Syntactic Sugar:
+        _
+        e
+        **
+*/
