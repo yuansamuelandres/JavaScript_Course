2cd46ff7ef31903e09d612726e7e99237715808c
yuansamuelandres <yuandeslytherinandres@gmail.com>
2024-07-19 21:36:52 +0200

    Committed the third JS Challenge

--- tutorial.js
+++ tutorial.js
@@ -1,32 +1,19 @@
 // In the name of Allah
 
-console.log(100..toString())
-console.log(100.156545.toFixed(2))
-console.log(parseInt("100 yes"))
-console.log(Number.isInteger(102.3))
-console.log(Number.isNaN(102.3))
+/* Challenge 3: Number Methods */
+let a = 1_00
+let b = 2_00.5
+let c = 1e2
+let d = 2.4
 
-console.log(Math.round(99.2))
-console.log(Math.round(99.5))
-console.log(Math.ceil(99.5))
-console.log(Math.floor(99.5))
-console.log(Math.trunc(99.5))
+console.log(Math.trunc(Math.min(a, b, c, d)))
 
-/* Number Methods:
-    toString()
-    toFixed()
-    parseInt()
-    parseFloat()
-    Number.isInteger()
-    Number.isNaN()
+console.log(a ** Math.floor(d))
 
-Math Objects:
-    round()
-    ceil()
-    floor()
-    min()
-    max()
-    pow(,)
-    random()
-    trunc()
-*/
+console.log(Math.trunc(d))
+console.log(Math.floor(d))
+console.log(parseInt(d.toFixed(0)))
+console.log(Math.round(d))
+
+console.log((Math.floor(b) / Math.ceil(d)).toFixed(2))
+console.log(Math.round(Math.floor(b) / Math.ceil(d)))
