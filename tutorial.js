d56c3eb210e265e55774ff18158eac86f6264528
yuansamuelandres <yuandeslytherinandres@gmail.com>
2024-08-05 18:28:12 +0200

    Added the tenth set of JS code notes

--- tutorial.js
+++ tutorial.js
@@ -1,17 +1,22 @@
 // In the name of Allah
 
-/* Challenge 4: String Methods */
-let a = "Elzero Web School"
+console.log(typeof "Yuan" === typeof "Muhammad")
 
-console.log(a.charAt(2).toUpperCase() + a.slice(3, 6))
+/* Operators:
+    Comparison...
+        ==
+        !=
 
-console.log(a.slice(-4, -3).toUpperCase().repeat(8))
+        ===
+        !===
 
-console.log(a.substring(0, 6).split())
-
-console.log(`${a.substr(0, 6)} ${a.substr(-6)}`)
-
-console.log(
-    a[0].toLowerCase() +
-    a.slice(1, a.length - 1).toUpperCase() +
-    a[a.length - 1].toLowerCase())
+        >
+        =>
+        <
+        =<
+    
+    Logical...
+        !
+        &&
+        ||
+*/
