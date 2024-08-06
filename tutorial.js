e6db30566b93af16c86616632ffadb3349c3a91c
yuansamuelandres <yuandeslytherinandres@gmail.com>
2024-08-06 16:33:38 +0200

    Added the twelveth set of JS code notes

--- tutorial.js
+++ tutorial.js
@@ -1,29 +1,23 @@
 // In the name of Allah
 
-/* Challenge 5: if Condition */
-let a = 10
-a < 10 ? console.log(10) :
-a>= 10 && a<= 40 ? console.log("10 To 40") :
-a > 40 ? console.log("> 40") :
-console.log("Unknown")
-
-let st = "Elzero Web School"
-if ((st.length * 2).toString() === "34") {
-    console.log("Good")
+let day = 0
+switch (day) {
+    case 0:
+        console.log("Saturday")
+        break
+    default:
+        console.log("Unknown")
 }
 
-if (st.slice(st.indexOf('W')-1, st.indexOf('W')+1).trim().toLowerCase() === "w") {
-    console.log("Good")
-}
-
-if (st !== "string") {
-    console.log("Good")
-}
-
-if (typeof st.length === "number") {
-    console.log("Good")
-}
-
-if (st.slice(st.indexOf('E'),st.indexOf('o')+1).repeat(2) === "ElzeroElzero") {
-    console.log("Good")
-}
+/* switch Statement:
+    switch (expression) {
+        cade 1:
+            code
+            break;
+        case 2:
+            code
+            break;
+        default:
+            code
+    }
+*/
