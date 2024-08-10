b217f803592a6b74fbf67ffdfe274982981345e8
yuansamuelandres <yuandeslytherinandres@gmail.com>
2024-08-10 18:32:41 +0200

    Committed the eighth JS Challenge

--- tutorial.js
+++ tutorial.js
@@ -1,25 +1,33 @@
 // In the name of Allah
 
-let products = ["Keyboard", "Mouse", "iPad"]
-let i = 0
+/* Challenge 8: Loops */
+let myAdmins = ["Ahmed", "Osama", "Sayed", "Stop", "Samera"]
+let myEmployees = ["Amgad", "Samah", "Ameer", "Omar", "Othman", "Amany", "Samia"]
 
-while (i < 10) {
-    console.log(i)
-    i++
-}
-
-do {
-    console.log(i)
-    i++
-} while (false)
-
-/* while loop:
-    while (condition) {
-        Block of code
+let member = 1
+let admins = myAdmins.length
+for (let i=0; i < myAdmins.length; i++) {
+    if (myAdmins[i] === "Stop") {
+        break
     }
+    admins = i + 1
+}
+document.write(`<div>We have ${admins} Admins</div><hr>`)
 
-do-while loop:
-    do {
-        Block of code
-    } while (condition)         will do the code for the first time no matter what
-*/
+mainloop: for (let j=0; j < myAdmins.length; j++) {
+    if (myAdmins[j] === "Stop") {
+        break
+    }
+    document.write(`<div>`)
+    document.write(`The Admin For Team ${j+1} Is ${myAdmins[j]}`)
+    document.write(`<h3>Team Members: </h3>`)
+    inside: for (let k=0; k < myEmployees.length; k++) {
+        if (myEmployees[k].startsWith(myAdmins[j].charAt(0))) {
+            document.write(`<p>- ${member} ${myEmployees[k]}</p>`)
+            member++
+        }
+    }
+    member = 1
+    document.write(`</div>`)
+    document.write(`<hr>`)
+}
