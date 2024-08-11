7fdf0238de3b7be529304e6bd2febf3412236a16
yuansamuelandres <yuandeslytherinandres@gmail.com>
2024-08-11 13:33:09 +0200

    Added the seventeenth set of JS code notes

--- tutorial.js
+++ tutorial.js
@@ -1,33 +1,20 @@
 // In the name of Allah
 
-/* Challenge 8: Loops */
-let myAdmins = ["Ahmed", "Osama", "Sayed", "Stop", "Samera"]
-let myEmployees = ["Amgad", "Samah", "Ameer", "Omar", "Othman", "Amany", "Samia"]
-
-let member = 1
-let admins = myAdmins.length
-for (let i=0; i < myAdmins.length; i++) {
-    if (myAdmins[i] === "Stop") {
-        break
-    }
-    admins = i + 1
+function userName(name = "Unknown") {
+    return `Hello, ${name}`
 }
-document.write(`<div>We have ${admins} Admins</div><hr>`)
+console.log(userName())
 
-mainloop: for (let j=0; j < myAdmins.length; j++) {
-    if (myAdmins[j] === "Stop") {
-        break
+/* Functions:   DRY Concept
+    function [Name](Parameter) {
+        BLock of code
+        return ...      interrupting the function
+        unreachable code
     }
-    document.write(`<div>`)
-    document.write(`The Admin For Team ${j+1} Is ${myAdmins[j]}`)
-    document.write(`<h3>Team Members: </h3>`)
-    inside: for (let k=0; k < myEmployees.length; k++) {
-        if (myEmployees[k].startsWith(myAdmins[j].charAt(0))) {
-            document.write(`<p>- ${member} ${myEmployees[k]}</p>`)
-            member++
-        }
-    }
-    member = 1
-    document.write(`</div>`)
-    document.write(`<hr>`)
-}
+    Parameters:
+        default => 
+            logical Or
+            condition
+            ES6
+        rest => ...Parameter        must be only one, and the last one
+*/
