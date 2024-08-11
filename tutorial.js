b0eb82b3aaca174e2192f30212549545f6e87774
yuansamuelandres <yuandeslytherinandres@gmail.com>
2024-08-11 15:41:27 +0200

    Committed the tenth JS Challenge

--- tutorial.js
+++ tutorial.js
@@ -1,36 +1,13 @@
 // In the name of Allah
 
-let print = () => 10
-console.log(print())
-
-var a = 1
-// let b = 2
-
-function showText() {
-    var a = 10          //var overrides the local scope
-    let b = 2
-    console.log(`From Global/function local ${a}`)
-    console.log(`From function local ${b}`)
+/* Challenge 10: Arrow Function */
+let names = (...name) => {
+    return `String ` + `[` + name.join("], [") + `] => Done !`
 }
+console.log(names("Osama", "Mohamed", "Ali", "Ibrahim"))
 
-showText()
-console.log(`From Global ${a}`)
-console.log(`From Global ${b}`)
-
-/* Anonymous Function:
-    can't be used before declaration
-
-Arrow Function: 
-    Only one line in the block of code
-
-Scope:
-    Global                      var
-    Local                       var
-    Block [if, switch, for]     let
-    Lexical                     nested functions
-*/
-/*
-! Search:
-    - Execution Context
-    - Lexical Environment
-*/
+let myNumbers = [20, 50, 10, 60]
+let calc = function (one, two, ...nums) {
+    return one + two + nums[Number(false)]
+}
+console.log(calc(10, myNumbers[Number(false)], myNumbers[Number(true)]))
