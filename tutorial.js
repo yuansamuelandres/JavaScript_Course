2c171cc8b061480cfe0874826aa6f26c3dc4aeca
yuansamuelandres <yuandeslytherinandres@gmail.com>
2024-08-11 13:58:44 +0200

    Added the eighteenth set of JS code notes

--- tutorial.js
+++ tutorial.js
@@ -1,20 +1,22 @@
 // In the name of Allah
 
-function userName(name = "Unknown") {
-    return `Hello, ${name}`
-}
-console.log(userName())
-
-/* Functions:   DRY Concept
-    function [Name](Parameter) {
-        BLock of code
-        return ...      interrupting the function
-        unreachable code
+/* Functions Practise */
+function showInfo(us="Un", ag="Un", rt=0, show="yes", ...sk) {
+    document.write(`<div>`)
+    document.write(`<h2>Welcome, ${us}</h2>`)
+    document.write(`<p>Age: ${ag}`)
+    document.write(`<p>Hour Rate: $${rt}`)
+    if (show === "yes") {
+        if (sk.length > 0) {
+            document.write(`<p>${sk.join(" | ")}</p>`)
+        }
+        else {
+            document.write(`<p>No skills</p>`)
+        }
     }
-    Parameters:
-        default => 
-            logical Or
-            condition
-            ES6
-        rest => ...Parameter        must be only one, and the last one
-*/
+    else {
+        document.write(`<p>Skills are hidden</p>`)
+    }
+    document.write(`</div>`)
+}
+showInfo("Yuan", 23, 0, "no")
