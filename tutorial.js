33a442f93f9f2d3674bb1d1db9e068e8a94c809a
yuansamuelandres <yuandeslytherinandres@gmail.com>
2024-08-12 19:49:06 +0200

    Added the thirtieth set of JS code notes

--- tutorial.js
+++ tutorial.js
@@ -1,24 +1,27 @@
 // In the name of Allah
 
-let myElement = document.createElement("div")
-let myAttribute = document.createAttribute("data-custom")
-let myText = document.createTextNode("Product One")
-let myComment = document.createComment("This is a comment")
+/* DOM [Create & Append Elements Practise] */
+for (let i=1; i<=100; i++) {
+    let myMainElement = document.createElement("div")
+    let mySubElement = document.createElement("div")
+    let myHeading = document.createElement("h2")
+    let myHeadingText = document.createTextNode(`Product Title ${i}`)
+    let myParagraph = document.createElement("p")
+    let myParagraphText = document.createTextNode("Product Description")
+    let seperator = document.createElement("hr") 
+    
+    myMainElement.className = "main-products"
+    mySubElement.className = `product ${i}`
+    
+    myHeading.appendChild(myHeadingText)
+    mySubElement.appendChild(myHeading)
+    
+    myParagraph.appendChild(myParagraphText)
+    mySubElement.appendChild(myParagraph)
+    
+    myMainElement.appendChild(mySubElement)
 
-myElement.className = "product"
-myElement.setAttributeNode(myAttribute)
-myElement.setAttribute("data-test", "Testing")
-
-myElement.appendChild(myComment)
-
-myElement.appendChild(myText)
-
-document.body.appendChild(myElement)
-
-/* DOM [Create & Append Elements]:
-    .createElement()
-    .createAttribute()
-    .setAttributeNode()
-    .setTextNode()
-    .appendChild()
-*/
+    document.body.appendChild(myMainElement)
+    document.body.appendChild(seperator)
+    console.log(myMainElement)
+}
