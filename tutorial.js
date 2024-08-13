2b258483fd4b2e9c68834fd957638d283aff149f
yuansamuelandres <yuandeslytherinandres@gmail.com>
2024-08-13 16:39:49 +0200

    Added the thirtieth-first set of JS code notes

--- tutorial.js
+++ tutorial.js
@@ -1,27 +1,26 @@
 // In the name of Allah
 
-/* DOM [Create & Append Elements Practise] */
-for (let i=1; i<=100; i++) {
-    let myMainElement = document.createElement("div")
-    let mySubElement = document.createElement("div")
-    let myHeading = document.createElement("h2")
-    let myHeadingText = document.createTextNode(`Product Title ${i}`)
-    let myParagraph = document.createElement("p")
-    let myParagraphText = document.createTextNode("Product Description")
-    let seperator = document.createElement("hr") 
-    
-    myMainElement.className = "main-products"
-    mySubElement.className = `product ${i}`
-    
-    myHeading.appendChild(myHeadingText)
-    mySubElement.appendChild(myHeading)
-    
-    myParagraph.appendChild(myParagraphText)
-    mySubElement.appendChild(myParagraph)
-    
-    myMainElement.appendChild(mySubElement)
 
-    document.body.appendChild(myMainElement)
-    document.body.appendChild(seperator)
-    console.log(myMainElement)
-}
+
+/* DOM [Deal with Children]:
+    .children
+    .childNodes
+    .firstChild
+    .lastChild
+    .firstElementChild
+    .lastElementChild
+
+DOM [Events]:
+    .onclick
+    .oncontextmenu      on mouse right click
+    .onmouseenter
+    .onmouseleave
+
+    .onload
+    .onscroll
+    .onresize
+
+    .onfocus
+    .onblur
+    .onsubmit
+*/
