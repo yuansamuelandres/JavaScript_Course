e75658b26fb8283115442f8f4c8e5420c5bf3174
yuansamuelandres <yuandeslytherinandres@gmail.com>
2024-08-14 18:15:07 +0200

    Added the thirtieth-fifth set of JS code notes

--- tutorial.js
+++ tutorial.js
@@ -2,18 +2,18 @@
 
 let element = document.getElementById("my-div")
 
-console.log(element.classList)
+element.style.color = "red"
+element.style.fontWeight = "bold"
+element.style.cssText = "color: green;"
 
-element.onclick = function () {
-    element.classList.toggle("test")
-}
+element.style.removeProperty("color")
+element.style.setProperty("font-size", "40px", "important")
 
-/* DOM [Class List]
-    classList
-        - length
-        - .contains()
-        - .item()
-        - .add()
-        - .remove()
-        - .toggle()
+document.styleSheets[0].rules[0].style.removeProperty("line-height")
+
+/* DON [CSS]:
+    - .style
+    - .cssText
+    - .removeProperty()
+    - .setProperty(,)
 */
