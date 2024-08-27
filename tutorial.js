d8b71d61c3b7918d913c2dd004e1f501488de626
yuansamuelandres <yuandeslytherinandres@gmail.com>
2024-08-27 18:07:40 +0200

    Added the fourtieth-sixth set of JS code notes

--- tutorial.js
+++ tutorial.js
@@ -1,73 +1,30 @@
 // In the name of Allah
 
-/* Challenge 13: BOM */
-let input = document.querySelector(".input")
-let submit = document.querySelector(".add")
-let tasksDiv = document.querySelector(".tasks")
+let myFreinds = ["Yuan", "Samuel", "Joel"]
+let [a, b, , c = "Andrew"] = myFreinds      // Destructing
+console.log(a)
+console.log(b)
+console.log(c)
 
-let arrayOfTasks = []
-if (localStorage.getItem("tasks")) {
-    arrayOfTasks = JSON.parse(localStorage.getItem("tasks"))
-}
+let frinds = ["Ahmed", "Sayed", "Ali"
+    , ["Shady", "Amr", ["Mohamed", "Gamal"]]]
+let [, , , [d, , [, e]]] = frinds
+console.log(d)
+console.log(e)
 
-getDataFromLocalStorage()
+let book = "Video"
+let video = "Book";
+// let x = book                 // old way of swapping
+// book = video
+// video = x
+// console.log(book)
+// console.log(video)
 
-document.onload = input.focus()
+[book, video] = [video, book]   // new way of swapping
+console.log(book)
+console.log(video)
 
-submit.onclick = function () {
-    if (input.value != "") {
-        addTaskToArray(input.value)
-        input.value = ""
-    }
-}
-tasksDiv.addEventListener("click", (e) => {
-    if (e.target.classList.contains("delete")) {
-        console.log(e.target.parentElement.getAttribute("task-id"))
-        deleteTaskWith(e.target.parentElement.getAttribute("task-id"))
-        e.target.parentElement.remove()
-    }
-})
-
-// Functions
-function addTaskToArray (t) {
-    const task = {
-        id: Date.now(),
-        title: t
-    }
-    arrayOfTasks.push(task)
-    addElementsToPageFrom(arrayOfTasks)
-    addDataToLocalStorageFrom(arrayOfTasks)
-}
-
-function addElementsToPageFrom (a) {
-    tasksDiv.innerHTML = ""
-    a.forEach((task) => {
-        let tasks = document.createElement("div")
-        tasks.className = "task"
-        tasks.setAttribute("task-id", task.id)
-        tasks.appendChild(document.createTextNode(task.title))
-        let Delete = document.createElement("button")
-        Delete.className = "delete"
-        Delete.appendChild(document.createTextNode("Delete"))
-        tasks.appendChild(Delete)
-        tasksDiv.appendChild(tasks)
-    })
-}
-
-function addDataToLocalStorageFrom (a) {
-    window.localStorage.setItem("tasks", JSON.stringify(a))
-}
-
-function getDataFromLocalStorage() {
-    let data = localStorage.getItem("tasks")
-    if (data) {
-        let tasks = JSON.parse(localStorage.getItem("tasks"))
-        addElementsToPageFrom(tasks)
-    }
-}
-
-function deleteTaskWith (id) {
-    arrayOfTasks = arrayOfTasks.filter((task) =>
-    task.id != id)
-    addDataToLocalStorageFrom(arrayOfTasks)
-}
+/* Destructing Arrays 
+Destructing = extracting data from arrays, objects, and maps 
+& setting them into new variables
+*/
