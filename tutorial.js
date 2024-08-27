42e57df92a25db8b3020d5e6506c51afabff9645
yuansamuelandres <yuandeslytherinandres@gmail.com>
2024-08-27 17:19:18 +0200

    Committed the thirteenth JS Challenge

--- tutorial.js
+++ tutorial.js
@@ -1,16 +1,73 @@
 // In the name of Allah
 
-window.localStorage.setItem("color", "red")
-window.sessionStorage.setItem("color", "blue")
+/* Challenge 13: BOM */
+let input = document.querySelector(".input")
+let submit = document.querySelector(".add")
+let tasksDiv = document.querySelector(".tasks")
 
-document.querySelector(".name").onblur = function () {
-    window.sessionStorage.setItem("input-name", this.value)
+let arrayOfTasks = []
+if (localStorage.getItem("tasks")) {
+    arrayOfTasks = JSON.parse(localStorage.getItem("tasks"))
 }
 
-/* BOM [Session Storage]        Duplicate Tab = Copy Session
-    setItem()
-    getItem()
-    removeItem()
-    clear()
-    key()
-*/
+getDataFromLocalStorage()
+
+document.onload = input.focus()
+
+submit.onclick = function () {
+    if (input.value != "") {
+        addTaskToArray(input.value)
+        input.value = ""
+    }
+}
+tasksDiv.addEventListener("click", (e) => {
+    if (e.target.classList.contains("delete")) {
+        console.log(e.target.parentElement.getAttribute("task-id"))
+        deleteTaskWith(e.target.parentElement.getAttribute("task-id"))
+        e.target.parentElement.remove()
+    }
+})
+
+// Functions
+function addTaskToArray (t) {
+    const task = {
+        id: Date.now(),
+        title: t
+    }
+    arrayOfTasks.push(task)
+    addElementsToPageFrom(arrayOfTasks)
+    addDataToLocalStorageFrom(arrayOfTasks)
+}
+
+function addElementsToPageFrom (a) {
+    tasksDiv.innerHTML = ""
+    a.forEach((task) => {
+        let tasks = document.createElement("div")
+        tasks.className = "task"
+        tasks.setAttribute("task-id", task.id)
+        tasks.appendChild(document.createTextNode(task.title))
+        let Delete = document.createElement("button")
+        Delete.className = "delete"
+        Delete.appendChild(document.createTextNode("Delete"))
+        tasks.appendChild(Delete)
+        tasksDiv.appendChild(tasks)
+    })
+}
+
+function addDataToLocalStorageFrom (a) {
+    window.localStorage.setItem("tasks", JSON.stringify(a))
+}
+
+function getDataFromLocalStorage() {
+    let data = localStorage.getItem("tasks")
+    if (data) {
+        let tasks = JSON.parse(localStorage.getItem("tasks"))
+        addElementsToPageFrom(tasks)
+    }
+}
+
+function deleteTaskWith (id) {
+    arrayOfTasks = arrayOfTasks.filter((task) =>
+    task.id != id)
+    addDataToLocalStorageFrom(arrayOfTasks)
+}
