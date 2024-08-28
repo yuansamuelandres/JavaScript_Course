b34840795420a32ead77b177f151637f621679ca
yuansamuelandres <yuandeslytherinandres@gmail.com>
2024-08-28 17:21:13 +0200

    Added the fiftieth set of JS code notes

--- tutorial.js
+++ tutorial.js
@@ -1,41 +1,18 @@
 // In the name of Allah
 
-/* Challenge 14: Destructing */
-let chosen = 3
-let myFriends = [
-    {title: "Osama", age: 39, available: true, skills: ["HTML", "CSS"]},
-    {title: "Ahmed", age: 25, available: false, skills: ["Python", "Django"]},
-    {title: "Sayed", age: 33, available: true, skills: ["PHP", "Laravel"]}
-]
+let myData = [1, 1, 1, 2, 3]
+let myUniqueData = new Set(myData)
 
-if (chosen === 1) {
-    let [{title: theName, age: theAge, available: x, skills:[, skill]}, , ] = myFriends
-    console.log(theName)
-    console.log(theAge)
-    if (x === true) {
-        console.log(`Available`)
-    } else {
-        console.log(`Not Available`)
-    }
-    console.log(skill)
-} else if (chosen === 2) {
-    let [, {title: theName, age: theAge, available: x, skills:[, skill]}, ] = myFriends
-    console.log(theName)
-    console.log(theAge)
-    if (x === true) {
-        console.log(`Available`)
-    } else {
-        console.log(`Not Available`)
-    }
-    console.log(skill)
-} else {
-    let [, , {title: theName, age: theAge, available: x, skills:[, skill]}] = myFriends
-    console.log(theName)
-    console.log(theAge)
-    if (x === true) {
-        console.log(`Available`)
-    } else {
-        console.log(`Not Available`)
-    }
-    console.log(skill)
-}
+console.log(myUniqueData)
+console.log(myUniqueData.delete(4)) // false
+
+/* Set Data Type = Set(Iterable)
+=> stores unique values only
+=> cannot access elements by index
+    .size
+
+    .add()
+    .delete()
+    .clear()
+    .has()
+*/
