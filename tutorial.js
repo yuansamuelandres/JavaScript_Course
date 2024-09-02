ae16b3d56601fd08dd5e0c58ff90c00fe0e090a4
yuansamuelandres <yuandeslytherinandres@gmail.com>
2024-09-02 17:30:34 +0200

    Added the fiftieth-second set of JS code notes

--- tutorial.js
+++ tutorial.js
@@ -1,28 +1,40 @@
 // In the name of Allah
 
-let mySet = new Set([1, 1, 1, 2, 3, "A", "A"])
-console.log(mySet)
+let myObject = {}
+let myEmptyObject = Object.create(null)
+let myMap = new Map()
+console.log(myObject)
+console.log(myEmptyObject)
+console.log(myMap)
 
-let iterator = mySet.keys()
-console.log(iterator.next())
-console.log(iterator.next().value)
+let myNewObject = {
+    10: "Number",
+    "10": "String",
+}
+console.log(myNewObject[10])
+let myNewMap = new Map()
+myNewMap.set(10, "Number")
+myNewMap.set("10", "String")
+console.log(myNewMap.get("10"))
 
-mySet.forEach((e) => console.log(e))
+/* Map Data Type vs Object:
+Map => doesn't contain keys by default
+Object => has keys by default
 
+Map => key can be anything
+Object => string or symbol keys 
 
-let myWS = new WeakSet([{A: 1, B: 2,}])
-console.log(myWS)
+Map => ordered by insertion
+Object => not 100% 
 
-let iter = myWS.keys()
-console.log(iter.next())
+Map => gets items by size
+Object => needs to be done manually
 
-/* WeakSet:
-=> You only store objects in it
-=> Can be garbage collected 
-becuase objects in it are held weakly (with no reference)
-=> Doesn't have the size property
-=> Doesn't have keys, values, nor entries
-=> Cannot use forEach()
+Map => can be directly iterated
+Object => not directly & needs to use .keys()
+
+Map => better performance when adding or removing data
+Object => low performance compared to Map
 */
 /*
 ! Search:
