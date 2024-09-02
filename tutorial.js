abda4652fcfe0c85bfe57082a95af4a085fe3993
yuansamuelandres <yuandeslytherinandres@gmail.com>
2024-09-02 17:40:09 +0200

    Added the fiftieth-third set of JS code notes

--- tutorial.js
+++ tutorial.js
@@ -1,38 +1,20 @@
 // In the name of Allah
 
-let myObject = {}
-let myEmptyObject = Object.create(null)
-let myMap = new Map()
-console.log(myObject)
-console.log(myEmptyObject)
-console.log(myMap)
+let myMap = new Map([
+    [10, "Number"],
+    ["Yuan", "String"],
+    [false, "Boolean"]
+])
 
-let myNewObject = {
-    10: "Number",
-    "10": "String",
-}
-console.log(myNewObject[10])
-let myNewMap = new Map()
-myNewMap.set(10, "Number")
-myNewMap.set("10", "String")
-console.log(myNewMap.get("10"))
+console.log(myMap.has(false))
 
-/* Map Data Type vs Object:
-Map => doesn't contain keys by default
-Object => has keys by default
+/* Map Methods:
+    set
+    get
+    delete
+    clear
+    has
 
-Map => key can be anything
-Object => string or symbol keys 
-
-Map => ordered by insertion
-Object => not 100% 
-
-Map => gets items by size
-Object => needs to be done manually
-
-Map => can be directly iterated
-Object => not directly & needs to use .keys()
-
-Map => better performance when adding or removing data
-Object => low performance compared to Map
+Properties:
+    size
 */
