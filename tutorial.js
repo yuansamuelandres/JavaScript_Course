1ff2d11450ba7f779c557ee10e98531b196fc9da
yuansamuelandres <yuandeslytherinandres@gmail.com>
2024-09-04 15:03:41 +0200

    Added the fiftieth-sixth set of JS code notes

--- tutorial.js
+++ tutorial.js
@@ -1,25 +1,12 @@
 // In the name of Allah
 
-console.log(Array.from("Yuan"))
-console.log(Array.from("123", function (n) {
-    return +n
-}))
-
-let myArray = [1, 1, 1, 2, 3, 4]
-// let mySet = new Set(myArray)
-// console.log(Array.from(mySet))
-console.log([...new Set(myArray)])    // Spread Operator
-
-function testArg () {
-    return Array.from(arguments)
-}
-console.log(testArg("Yuan", "Samuel"))
+let myArray = [10, 20, 30, 40, 50, "A", "B"]
+// myArray.copyWithin(3) //[10, 20, 30, 10, 20, 30, 40]
+// myArray.copyWithin(4, -1)  //[10, 20, 30, 40, "B", "A", "B"]
+// myArray.copyWithin(1, -2)  //[10, "A", "B", 40, 50, "A", "B"]
+myArray.copyWithin(1, -2, -1)  //[10, "A", 30, 40, 50, "A", "B"]
+console.log(myArray)
 
 /* Array Methods:
-    Array.from(Iterable, MapFunction, This)
-        - Variable
-        - String Numbers
-        - Set
-        - Using the Map Function
-        - arguments usage inside the function
+    Array.copyWithin(Target, Start, End)
 */
