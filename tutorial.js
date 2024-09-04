486766bcffa49d76c013d538557a4f092df130cb
yuansamuelandres <yuandeslytherinandres@gmail.com>
2024-09-04 18:10:40 +0200

    Committed the fifteenth JS Challenge

--- tutorial.js
+++ tutorial.js
@@ -1,37 +1,9 @@
 // In the name of Allah
 
-console.log([..."Yuan"])
+/* Challenge 15: Map & Set */
+let n1 = [10, 30, 10, 20]
+let n2 = [30, 20, 10]
 
-// Concatenation
-let array1 = [1, 2, 3]
-let array2 = [4, 5, 6]
-let allArrays = [...array1, ...array2]
-console.log(allArrays)
+console.log(Math.max(...n2) * [...n1, ...n2].length)   // 210
 
-// Objects
-let ob1 = {
-    a: 1,
-    b: 2,
-}
-let ob2 = {
-    c: 3,
-    d: 4,
-}
-console.log({...ob1, ...ob2, e: 5})
-
-// Copying
-let copy = [...array1]
-console.log(copy)
-
-// Push 
-let allFriends = ["Yuan", "Samuel"]
-let thisYear = ["Andrew"]
-allFriends.push(...thisYear)
-console.log(allFriends)
-
-// Math Object
-let nums = [10, 20, -200, 500]
-console.log(Math.max(...nums))
-
-/* Spread Operator => ...Iterable
-*/
+// 70 * 6 = 420 / 2 = 210
